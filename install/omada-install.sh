cd3652fcba46d72032dbaa12a2edd6b820a16afb
Hafizul Islam Reyad <reyad@knights.ucf.edu>
2025-05-30 09:38:19 -0400

    Omada: mongodb4.4 install

--- install/omada-install.sh
+++ install/omada-install.sh
@@ -40,6 +40,14 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | $STD apt-key add 
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" |
   $STD tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 $STD apt-get update
+
+$STD apt-get install -y \
+  mongodb-org=4.4.24 \
+  mongodb-org-server=4.4.24 \
+  mongodb-org-shell=4.4.24 \
+  mongodb-org-mongos=4.4.24 \
+  mongodb-org-tools=4.4.24
+
 msg_ok "Installed MongoDB $MONGODB_VERSION"
 
 msg_info "Installing Omada Controller"
